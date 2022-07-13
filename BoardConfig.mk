3d8e471f7d3d38b6ced9b6aad3ea950ce9529c3a
Ankit Savani <ankitmsavani12345@gmail.com>
2022-07-13 14:01:14 +0000

    vince: TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vince
 TARGET_KERNEL_VERSION := 4.9
